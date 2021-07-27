85ef11a47e5ce673b121919fba2bd901b79b90f7
nitachang <80136827+nitachang@users.noreply.github.com>
2021-07-27 11:39:27 +0800

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 10:58:48 2021
+
+@author: nitac
+"""
+
+for x in [2, 4, 10, 20]:
+    print('x:',x)
+    
+    
+for x in ('Iamharry'):
+    print('x:',x)
+    
+print('-----------------')
+    
+for y in range(11):
+    print('y',y)
+    
+print('-----------------')
+
+for x in range(7,9):
+    print(x)
+    
+print('-----------------')
+
+for z in range(2, 10, 2):
+    print(z)
+    
+print('-----------------')  
+
+for x in range(1, 11):
+    print('*' * x)
+    
+print('-----------------')
+ 
+for z in range(1, 6):
+    print('*' * (6-z))
+
+print('-----------------')
+
+
+
+
+
+
+
+    
+
+
